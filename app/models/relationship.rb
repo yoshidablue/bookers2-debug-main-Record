75c566e906dad6d87b294f577c9941a9ca087d8b
yoshidablue <sugar.blue.1224@gmail.com>
2022-11-18 05:09:52 +0000

    bookers2-debug 基本形作成 完了

---
+++ app/models/relationship.rb
@@ -0,0 +1,9 @@
+class Relationship < ApplicationRecord
+
+  # belongs_to :userとするとどっちがどっちのuserかわからなくなんるので,followerとfollowedで分けている。
+  # class_name: "User"でuserテーブルからデータをとってくる
+  belongs_to :follower, class_name: "User"
+  belongs_to :followed, class_name: "User"
+
+
+end
