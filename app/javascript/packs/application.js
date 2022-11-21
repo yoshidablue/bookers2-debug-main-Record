6bd351d54e71142b51d17bec69af67b43df7fc5b
yoshidablue <sugar.blue.1224@gmail.com>
2022-11-21 07:29:34 +0000

    過去７日分の投稿数グラフ化

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,10 @@ import "jquery"
 import "popper.js"
 import "bootstrap"
 import '@fortawesome/fontawesome-free/js/all';
-import "../stylesheets/application" 
+import "../stylesheets/application"
+
+require("chartkick")
+require("chart.js")
 
 Rails.start()
 Turbolinks.start()
--- app/models/book.rb
+++ app/models/book.rb
@@ -7,10 +7,16 @@ class Book < ApplicationRecord
   has_many :favorites,     dependent: :destroy
   has_many :book_comments, dependent: :destroy
 
+  # 過去７日間の投稿数
   # 今日の投稿数
   scope :created_today,     -> {where(created_at: Time.zone.now.all_day)}
   # 昨日の投稿数
   scope :created_yesterday, -> {where(created_at: 1.day.ago.all_day)}
+  scope :created_2days,     -> {where(created_at: 2.days.ago.all_day)}
+  scope :created_3days,     -> {where(created_at: 3.days.ago.all_day)}
+  scope :created_4days,     -> {where(created_at: 4.days.ago.all_day)}
+  scope :created_5days,     -> {where(created_at: 5.days.ago.all_day)}
+  scope :created_6days,     -> {where(created_at: 6.days.ago.all_day)}
   # 今週の投稿数
   scope :created_this_week, -> {where(created_at: 6.day.ago.beginning_of_day..Time.zone.now.end_of_day)}
   # 先週の投稿数
